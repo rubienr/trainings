fd1456a33ecbe7f1143d15b7c3b6c405207f9159
Raoul Rubien <raoul.rubien@adbsafegate.com>
2020-07-24 12:20:38 +0200

    updated scripts

--- shell-commands/compile.sh
+++ shell-commands/compile.sh
@@ -2,7 +2,7 @@
 
 echo "crating table of contents ..."
 echo -en "*document heavily under construction*\n\n# Table of Contents\n" >  README.md
-awk --file get_md_toc.awk content.md >> README.md
+awk --file extract_toc.awk content.md >> README.md
 echo "---"  >> README.md
 echo "adding content ..."
 cat content.md >> README.md
